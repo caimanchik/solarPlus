7a6ffb6506d74dd6614721246a932ef816acbc39
caiman <shimyshimya@mail.ru>
2023-02-10 16:55:55 +0500

    final dev

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,7 @@
 import * as functions from "./modules/functions.js"
 
 functions.isWebp();
+functions.reply()
 
+let greenSlider = new functions.greenSLider()
+let clientsSlider = new functions.clientsSLider()
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -12,3 +12,108 @@ export function isWebp() {
         document.documentElement.classList.add(className)
     });
 }
+
+
+export function reply() {
+    let footer = document.querySelector('.footer').getBoundingClientRect().top;
+    let links = document.querySelectorAll('.scroll');
+    
+    links.forEach(link => link.addEventListener('click', e => {
+        e.preventDefault()
+        
+        window.scrollBy({
+            top: footer, 
+            behavior: 'smooth'
+        })
+    }))
+}
+
+export class greenSLider {
+    
+    constructor(params) {
+        this.slider = document.querySelector('.green-slider');
+        this.slides = document.querySelectorAll('.green-slide');
+        this.next = document.querySelector('.green-slider__arrow-next');
+        this.prev = document.querySelector('.green-slider__arrow-before');
+        
+        this.selected = 0
+        this.changeSlide()
+        this.arrowLogic()
+    }
+    
+    changeSlide() {
+        this.slides.forEach((slide, i) => {
+            slide.classList.remove('visible')
+            
+            if (i === this.selected) {
+                slide.classList.add('visible')
+            }
+        })
+    }
+    
+    arrowLogic() {
+        this.next.addEventListener('click', e => {
+            this.selected++
+            
+            if (this.selected + 1 > this.slides.length) 
+                this.selected = 0
+            
+            this.changeSlide()
+        })
+        
+        this.prev.addEventListener('click', e => {
+            this.selected--
+
+            if (this.selected < 0)
+                this.selected = this.slides.length - 1
+
+            this.changeSlide()
+        })
+    }
+}
+
+export class clientsSLider {
+    
+    constructor(params) {
+        this.slider = document.querySelector('.clients-slider');
+        this.slides = document.querySelectorAll('.clients-slide');
+        this.next = document.querySelector('.clients-slider__arrow-next');
+        this.prev = document.querySelector('.clients-slider__arrow-before');
+        
+        this.selected = 0
+        this.changeSlide()
+        this.arrowLogic()
+    }
+    
+    changeSlide() {
+        this.slides.forEach((slide, i) => {
+            slide.classList.remove('visible')
+            
+            if (i === this.selected) {
+                slide.classList.add('visible')
+            }
+        })
+    }
+    
+    arrowLogic() {
+        this.next.addEventListener('click', e => {
+            this.selected++
+            
+            if (this.selected + 1 > this.slides.length) 
+                this.selected = 0
+            
+            this.changeSlide()
+        })
+        
+        this.prev.addEventListener('click', e => {
+            this.selected--
+
+            if (this.selected < 0)
+                this.selected = this.slides.length - 1
+
+            this.changeSlide()
+        })
+    }
+}
+
+
